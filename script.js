0c8d4efc5058155ff96b77b118a9e4c78e530ce7
Glitch (tre-nyc-trvl) <none>
2024-07-19 18:55:17 +0000

    🥍🫔 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,44 @@
+M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
+
+
+console.log('hi');
+
+
+
+
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+$(document).ready(function(){
+ $('.dropdown-trigger').dropdown(
+ { hover: true
+ }
+ );
+});
+ $(document).ready(function(){
+   $('.sidenav').sidenav();
+ });
+
